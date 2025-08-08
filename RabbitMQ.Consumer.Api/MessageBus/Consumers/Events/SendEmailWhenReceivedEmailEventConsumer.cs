fc205d444a4990acb307489b34c759714a6577ba
hieucbui <buihieudl@gmail.com>
2025-08-08 20:25:27 +0700

    update

---
+++ RabbitMQ.Consumer.Api/MessageBus/Consumers/Events/SendEmailWhenReceivedEmailEventConsumer.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using RabbitMQ.Consumer.Api.Abstractions.Messages;
+using RabbitMQ.Contract.Abstractions.IntergrationEvents;
+
+namespace RabbitMQ.Consumer.Api.MessageBus.Consumers.Events
+{
+    public class SendEmailWhenReceivedEmailEventConsumer : Comsumer<DomainEvent.EmailNotificationEvent>
+    {
+        public SendEmailWhenReceivedEmailEventConsumer(ISender sender) : base(sender)
+        {
+        }
+    }
+}
